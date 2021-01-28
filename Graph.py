c9ba9c947e2bb8fcafd98481b0526a7bb665f205
Alex Borzetsov <borzetsov@gmail.com>
2021-01-28 14:15:50 +0700

    Update Graph.py

--- Graph.py
+++ Graph.py
@@ -1,12 +1,21 @@
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 fig, ax = plt.subplots()
+sin_l, = ax.plot(np.sin(0))
+cos_l, = ax.plot(np.cos(0))
+ax.set_ylim(-1, 1)
+ax.set_xlim(0, 5)
+dx = 0.01
 
-x = np.linspace(-5, 5, 100)
-y = x**2 / np.sin(x/2)
-z = np.sin(x)
+def update(i):
+    # i is a counter for each frame.
+    # We'll increment x by dx each frame.
+    x = np.arange(0, i) * dx
+    sin_l.set_data(x, np.sin(x))
+    cos_l.set_data(x, np.cos(x))
+    return sin_l, cos_l
 
-ax.plot(x,y,z)
-
+ani = animation.FuncAnimation(fig, update, frames=500, interval=20)
 plt.show()
